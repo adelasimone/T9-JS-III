9a6b77715e0b1f05f9e7f6af8e330ec4c5adf912
Adela Simone <adelasimoneac@hotmail.com>
2020-05-28 11:57:29 -0300

    Atualizei minha resoluções

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,19 +1,10 @@
 // map(): faz uma iteração com todos os elementos da lista e retorna outra
-// map():
-// input: Array
-// output: Array de mesmo tamanho
-
 // 1) Adicionar "a" no final das palavras
-const profissionais = [
-  "desenvolvedor",
-  "programador",
-  "autor",
-  "vereador",
-  "professor",
-];
+const profissionais = ["desenvolvedor", "programador", "autor", "vereador", "professor"];
 
 const profissionaisMelhores = profissionais.map(
   (item) => {
+    // item são os itens litaralmente do array, pode muda o nome dele tb
     return item + "a";
   }
 );
@@ -21,64 +12,132 @@ const profissionaisMelhores = profissionais.map(
 // 2) Retornar frutas com somente a primeira letra maiúscula
 const frutas = ["maçã", "banana", "Pera", "Melancia", "pêsSSEgO", "jAcA"];
 
-// retorno:
-// ["Maçã", "Banana", "Pera", "Melancia", "Pêssego", "Jaca"]
-
-// passar por todos os itens:
-// 1. colocar todas as letras minúsculas
-// 2. colocar a primeira letra maiúscula
-// 3. colocar a lista nova em uma variável
-
 const frutasFormatadas = frutas.map(
   (item) => {
-    const letraInicial = item[0].toUpperCase();
-    const itemMinusculo = item.toLowerCase();
-    const itemSemPrimeiraLetra = itemMinusculo.substring(1);
+    const letraInicial = item[0].toUpperCase(); // pega a primeira letra e deixa maiuscula
+    const itemMinusculo = item.toLowerCase();  //deixa tudo minusculo
+    const itemSemPrimeiraLetra = itemMinusculo.substring(1);  // pega o "resto" da palavra
 
-    return letraInicial + itemSemPrimeiraLetra;
+    return letraInicial + itemSemPrimeiraLetra;  //concatena os dois 
+
+    // let novaFruta = item[0].toUpperCase() + item.toLowerCase().substring(1);
+    // novaFruta[0] = novaFruta[0].toUpperCase();
+    // return novaFruta;
   }
 );
+
+// retornar uma nova array com o nome das alunas com a nota final
+const notasAlunas = [
+  {
+    nome: "Julia", notas: [
+      { materia: "matemática", nota: 7, peso: 1.5 },
+      { materia: "portugues", nota: 3, peso: 2 }
+    ]
+  },
+  {
+    nome: "Zelia", notas: [
+      { materia: "matemática", nota: 6, peso: 1.5 },
+      { materia: "portugues", nota: 5, peso: 2 }
+    ]
+  },
+  {
+    nome: "Jussara", notas: [
+      { materia: "matemática", nota: 8, peso: 1.5 },
+      { materia: "portugues", nota: 2, peso: 2 }
+    ]
+  }
+]
+
+const notasFinaisAlunas = notasAlunas.map(
+  (aluna) => {
+    // console.log(aluna.nome);
+    const arrayNotas = aluna.notas;
+    // input: array
+    // output: n°
+
+    let somaNotas = 0;
+    let somaPesos = 0;
+
+    for (let i = 0; i < arrayNotas.length; i++) {
+      const nota = arrayNotas[i].nota;
+      const peso = arrayNotas[i].peso;
+      const notaFinalMateria = nota * peso;
+      // console.log(arrayNotas[i].materia, notaFinalMateria)
+      somaNotas += notaFinalMateria;
+      somaPesos += peso;
+    }
+    const notaFinalUm = somaNotas / somaPesos;
+    const novaAluna = {
+      nome: aluna.nome,
+      notaFinal: notaFinalUm
+    }
+    return novaAluna
+  }
+)
+
+// 3) Criar uma array somente com as alunas que passaram de ano (média > 5)
+// notasFinasiAlunas
+
+// const alunasPassantes = [{ nome: "elia", nota:5}]
+
+const alunasPassantes = notasFinaisAlunas.filter(
+  (item) => {
+    // console.log(item.notaFinal)
+    const media = 5;
+    const ehMaiorMedia = item.notaFinal >= media;
+    return ehMaiorMedia
+
+    // MINHA RESOLUÇÃO
+    // const passou = item.notaFinal >= 5
+    // return passou;
+  }
+)
 
 // filter(): retorna uma nova array com apenas com elementos que atendem à condição
 // 1) Criar nova array somente com números pares
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// [2, 4, 6, 8, 10]
 
-const numPares = num.filter(
-  (bolinha) => {
-    const ehNumPar = bolinha % 2 === 0;
-    return ehNumPar;
-  }
-)
 
 // 2) Criar nova array somente com profissionais da área de TI
 const pessoas = [
   { nome: "Mell", area: "TI", profissao: "dev" },
-  { nome: "Munique", area: "ciência", profissao: "pesquisadora"},
-  { nome: "Melissa", area: "construção", profissao: "engenheira"},
+  { nome: "Munique", area: "ciência", profissao: "pesquisadora" },
+  { nome: "Melissa", area: "construção", profissao: "engenheira" },
   { nome: "Madalena", area: "TI", profissao: "infra" },
-  { nome: "Monica", area: "medicina", profissao: "cardiologista"},
+  { nome: "Monica", area: "medicina", profissao: "cardiologista" },
 ];
 
-// expectativa: 
-// const profissionaisTI = [
-//   { nome: "Mell", area: "TI", profissao: "dev" },
-//   { nome: "Madalena", area: "TI", profissao: "infra" }
-// ];
-
-const profissionaisTI = pessoas.filter(
+const todosProfissionais = pessoas.filter(
   (item) => {
-    const ehDeTI = item.area === "TI";
-    return ehDeTI;
+    const areaTI = item.area === "TI"
+    return areaTI;
   }
 )
 
+
 // reduce(): recebe uma array e retorna um valor só. Redutor.
 // 1) Dado uma array de números, retornar a soma de todos eles
+// usamos a array do  filter
+// const num = [1, 2,  3,  4,   5, 6, 7, 8, 9, 10];
+//               1+2=3 |   |     |
+//                   3+3=6 |    |
+//                       6+4=10 |
+//                           10+5=15 ...
+// const soma = num.reduce(
+//   // (acumulado, item. indice, array)
+// (acumulado, item) =>{
+//   console.log('acc', acumulado);
+//   console.log('item', item);
+//   const somaAtual = acumulado + item;
 
+//   return somaAtual;
+// }
+// )
 
 // 2) Retornar o lucro de uma empresa
+// se for saída, subtrair o valor
+// se for entrada, adicionar o valor
 const transacoes = [
   { descricao: "carro", valor: 32000, tipo: "saída" },
   { descricao: "cliente 1", valor: 10000, tipo: "entrada" },
@@ -86,8 +145,30 @@ const transacoes = [
   { descricao: "salario 1", valor: 8500, tipo: "saída" }
 ];
 
+const lucro = transacoes.reduce(
+  (acumulado, item) => {
+    // console.log('aa', acumulado)
+    // console.log('bb', item)
+    if(item.tipo === "entrada") {
+      return acumulado + item.valor
+    } else {
+      return acumulado - item.valor
+    }
+  }, 0  // --- OU ESSA
+)
+
+//  --- USAR ESSA VALIDAÇÃO (pra depois da primeira rodada não ficar masi como obj e acabr ficando NaN)
+  //   if (typeof acumulado === "object") {
+  //     return acumulado.valor + item.valor
+  //   }
+  //     return acumulado + item.valor;
+  // }
+
+
 
 // sort(): organiza itens da array de acordo com uma verificação
 // 1) Organizar números de forma decrescente
 
 // 2) Organizar pessoas por ordem alfabética
+
+
