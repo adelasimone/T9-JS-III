1046c4f305105e57bb8e558a6e3c277f897443cd
Adela Simone <adelasimoneac@hotmail.com>
2020-05-29 14:33:04 -0300

    Exercício CEP

--- 02-xhr/exercicio/script.js
+++ 02-xhr/exercicio/script.js
@@ -9,27 +9,33 @@ const input = document.getElementById('pkmInput');
 const pkmPlaceholder = document.getElementById('pkmPlaceholder');
 
 
-const chamarRequisicao = (e) => {
-  e.preventDefault();
-  const requisito = new XMLHttpRequest();
-  const verbo = "GET"
-  const nomePkm = input.value
-  const url = `https://pokeapi.co/api/v2/pokemon/${nomePkm}/`
+// const chamarRequisicao = (e) => {
+//   e.preventDefault();
+//   const requisito = new XMLHttpRequest();
+//   const verbo = "GET"
+//   const nomePkm = input.value // guarda o input pra usar no link da const url
+//   const url = `https://pokeapi.co/api/v2/pokemon/${nomePkm}/`
 
-  requisito.open(verbo, url);
+//   requisito.open(verbo, url);
 
-  requisito.addEventListener("readystatechange", () => {
-    if (requisito.readyState === 4 && requisito.status === 200) {
-      const guarda = JSON.parse(requisito.response) // transforma e guarda string JSON em objeto pra manipularmos
-      pkmPlaceholder.innerText = guarda.name
-      const img = document.createElement('img');
-      img.setAttribute('src', `https://pokeres.bastionbot.org/images/pokemon/${guarda.id}.png`);
-      pkmPlaceholder.appendChild(img)
-    } else {
-      chamarRequisicao.textContent = "API sem resposta";
-    }
-  })
-  requisito.send();
-};
+//   requisito.addEventListener("readystatechange", () => {
+//     if (requisito.readyState === 4 && requisito.status === 200) {
+//       const guarda = JSON.parse(requisito.response) // transforma e guarda string JSON em objeto pra manipularmos
+//       pkmPlaceholder.innerText = guarda.name
+//       const img = document.createElement('img');
+//       img.setAttribute('src', `https://pokeres.bastionbot.org/images/pokemon/${guarda.id}.png`);
+//       pkmPlaceholder.appendChild(img)
+//     } else {
+//       chamarRequisicao.textContent = "API sem resposta";
+//     }
+//   })
+//   requisito.send();
+// };
 
-form.addEventListener('submit', (e) => chamarRequisicao(e));
+// form.addEventListener('submit', (e) => chamarRequisicao(e));
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${nomePkm}/`)
+    .then((response) => { return response.json() })
+    .then(data => {
+        console.log('data: ', data)
+    })
